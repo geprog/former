fde7e27864d3a18897fec8d2ad0d0c77e72dbddf
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-04-18 18:20:07 +0200

    rm schema repo

--- packages/former/src/components/formFieldOptions.ts
+++ packages/former/src/components/formFieldOptions.ts
@@ -1,6 +1,5 @@
 import type { FormKitSchemaNode } from '@formkit/core';
-import textSchema from '~/../../input-schemas/schemas/text.json';
 
 export const formFieldOptionSchemas = {
-  text: textSchema as FormKitSchemaNode[],
+  text: [] as FormKitSchemaNode[],
 };
