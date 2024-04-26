2711d56e8e57d256bb3a9a562ae2ae8aaa820c8d
ShahwarZia <shahwarzia@live.com>
2024-04-26 11:39:59 +0000

    fix: use focus trap properly

--- packages/former/src/components/FormFieldSelector.vue
+++ packages/former/src/components/FormFieldSelector.vue
@@ -1,32 +1,26 @@
 <template>
-  <div
-    v-if="isFormFieldSelectorOpen"
-    role="dialog"
-    class="fixed inset-0 z-20 flex items-center justify-center bg-gray-900 bg-opacity-50"
-  >
-    <UseFocusTrap>
+  <UseFocusTrap v-if="isFormFieldSelectorOpen" :options="{ immediate: true }">
+    <div role="dialog" class="fixed inset-0 z-20 flex items-center justify-center bg-gray-900 bg-opacity-50">
       <div ref="formFieldSelector" class="rounded bg-white p-8 shadow-md">
-        <div class="text-center">
-          <p class="mb-10 text-4xl">Select the type of input field</p>
-          <div class="flex flex-col items-center">
-            <FormKit
-              type="button"
-              v-for="type in availableFieldTypes"
-              :key="type"
-              :label="`select ${type} as type for the new input field`"
-              @click="selectType(type)"
-            >
-              <span class="text-2xl">{{ type }}</span>
-            </FormKit>
-          </div>
+        <p class="mb-10 text-4xl">Select the type of input field</p>
+        <div class="flex flex-col items-center">
+          <FormKit
+            type="button"
+            v-for="type in availableFieldTypes"
+            :key="type"
+            :label="`select ${type} as type for the new input field`"
+            @click="selectType(type)"
+          >
+            <span class="text-2xl">{{ type }}</span>
+          </FormKit>
         </div>
       </div>
-    </UseFocusTrap>
-  </div>
+    </div>
+  </UseFocusTrap>
 </template>
 <script setup lang="ts">
 import { UseFocusTrap } from '@vueuse/integrations/useFocusTrap/component';
-import { availableFieldTypes } from './formFieldOptions';
+import { availableFieldTypes } from './formFieldSchema';
 import { useConfirmDialog, onClickOutside } from '@vueuse/core';
 import { ref, watch } from 'vue';
 
@@ -54,7 +48,6 @@ watch(isSelectorOpen, async () => {
     emits('selectedType', elementType);
   }
 });
-
 onClickOutside(formFieldSelector, () => {
   closeFormFieldSelector();
   isSelectorOpen.value = false;
