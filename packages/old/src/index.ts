c199fce5db51805a5e4e9327030931c82ae0a66c
Anbraten <anton@ju60.de>
2024-09-03 17:05:04 +0200

    rm old

--- packages/old/src/index.ts
+++
@@ -1,8 +0,0 @@
-import './index.css';
-
-import FormBuilder from './components/FormBuilder.vue';
-import FormContent from './components/FormContent.vue';
-import FormFieldOptions from './components/FormFieldOptions.vue';
-import { type FormFieldType, baseOptions } from './components/formFieldTypes';
-
-export { FormBuilder, FormContent, FormFieldOptions, type FormFieldType, baseOptions as formFieldBaseOptions };
--- packages/old/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.vue', './formkit.theme.ts', './formkit.config.ts'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
