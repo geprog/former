a2fb62a09666b8691d115c1fbfa664c6a1fe837e
mariusheine <m.heine@geprog.com>
2024-09-19 18:48:23 +0000

    improve styling of select component

--- packages/former/src/sample/Select.vue
+++ packages/former/src/sample/Select.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-col">
     <label class="p-1" v-if="label">{{ label }}</label>
-    <select v-model="modelValue">
-      <option v-for="(item, i) in options" :key="i">{{ item.label }}</option>
-    </select>
+    <div class="border rounded">
+      <select v-model="modelValue" class="w-full p-1 rounded bg-[field]" >
+        <option v-for="(item, i) in options" :key="i">{{ item.label }}</option>
+      </select>
+    </div>
   </div>
 </template>
 
