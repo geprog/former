3472a5496cc9fdcdb203b7f9e5606edcda7a4d52
mariusheine <m.heine@geprog.com>
2024-09-20 11:15:44 +0000

    fix linter

--- packages/former/vite.config.playground.ts
+++ packages/former/vite.config.playground.ts
@@ -1,9 +1,6 @@
-import { resolve } from 'node:path';
 import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
