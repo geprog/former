3fe49d3fcd8636e111477a3cba910237924846a6
mariusheine <m.heine@geprog.com>
2024-09-20 07:13:27 +0000

    fix select option values

--- packages/former/src/sample/Select.vue
+++ packages/former/src/sample/Select.vue
@@ -3,7 +3,7 @@
     <label v-if="label" class="p-1">{{ label }}</label>
     <div class="border rounded">
       <select v-model="modelValue" class="w-full p-1 rounded bg-[field]">
-        <option v-for="(item, i) in options" :key="i">
+        <option v-for="(item, i) in options" :key="i" :value="item.value">
           {{ item.label }}
         </option>
       </select>
