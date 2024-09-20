dfcb97baad04aad005de4404ada208f954591675
Tristan Stier <t.stier@geprog.com>
2024-09-20 12:04:02 +0000

    new release 46

--- packages/former/src/index.ts
+++ packages/former/src/index.ts
@@ -4,14 +4,17 @@ import FormContent from '~/components/FormContent.vue';
 import Former from '~/components/Former.vue';
 import FormNodeProps from '~/components/FormNodeProps.vue';
 import FormRenderer from '~/components/FormRenderer.vue';
-import type { FormData, FormFieldType, Mode, SchemaNode, ShowIfPredicate, Validator } from '~/types';
+import type { FormData, FormerProps, FormFieldType, Mode, SchemaNode, ShowIfPredicate, Validator } from '~/types';
+import FormDragContainer from './components/FormDragContainer.vue';
 import './index.css';
 
 export {
   FormAdd,
   FormContent,
   type FormData,
+  FormDragContainer,
   Former,
+  type FormerProps,
   type FormFieldType,
   FormNodeProps,
   FormRenderer,
