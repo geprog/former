962551c54b32e0cc9d442a8dce46875b87365e6f
Marius Heine <m.heine@geprog.com>
2025-02-11 11:46:10 +0100

    fix: node validation check does not get injected values (#70)

--- packages/former/src/compositions/useSchema.ts
+++ packages/former/src/compositions/useSchema.ts
@@ -1,18 +1,16 @@
 import type { FormComponents, InternalSchemaNode, Validator } from '~/types';
 import { computed, type Ref } from 'vue';
 import { schemaIterator } from '~/utils';
-import { inject } from './injectProvide';
 
-export function isNodeValid(node: InternalSchemaNode, config?: { validator: Validator, components: FormComponents }) {
-  const { validator, components } = config || { validator: inject('validator'), components: inject('components') };
+export function isNodeValid(node: InternalSchemaNode, validator: Validator, components: FormComponents) {
   const propsSchema = components[node.type].propsSchema;
   return propsSchema.every(propsNode =>
     validator(propsNode, propsNode.name === '$name' ? node.name : (node.props || {})[propsNode.name || '']) === true);
 }
 
-export default function useSchema(schema: Ref<InternalSchemaNode[]>, config: { validator: Validator, components: FormComponents }) {
+export default function useSchema(schema: Ref<InternalSchemaNode[]>, validator: Validator, components: FormComponents) {
   const isValid = computed(() => {
-    return [...schemaIterator(schema.value)].every(node => isNodeValid(node, config));
+    return [...schemaIterator(schema.value)].every(node => isNodeValid(node, validator, components));
   });
 
   return { isValid };
