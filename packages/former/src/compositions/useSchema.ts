862db3a6172eaf84a68c0f7bd164de3744c071a6
Marius Heine <m.heine@geprog.com>
2025-02-11 11:39:20 +0100

    remove unused import

--- packages/former/src/compositions/useSchema.ts
+++ packages/former/src/compositions/useSchema.ts
@@ -1,7 +1,6 @@
 import type { FormComponents, InternalSchemaNode, Validator } from '~/types';
 import { computed, type Ref } from 'vue';
 import { schemaIterator } from '~/utils';
-import { inject } from './injectProvide';
 
 export function isNodeValid(node: InternalSchemaNode, validator: Validator, components: FormComponents) {
   const propsSchema = components[node.type].propsSchema;
