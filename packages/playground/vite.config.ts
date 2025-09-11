d0e9a352be507b2d97730dd74b2582c09e1856f5
AmmarKaraja <ammarkaraja@outlook.de>
2025-09-11 11:10:04 +0000

    update: delete unimportant comments

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -12,12 +12,10 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    // Erlaube deinen Gitpod-Host (genau so wie in der Fehlermeldung)
     allowedHosts: [
       '5173-ammarkaraja-former-08kqzylvsw0.ws-eu121.gitpod.io',
-      '.gitpod.io', // optional: gesamte Domainfamilie erlauben
+      '.gitpod.io',
     ],
-    // (optional, oft nötig in Gitpod, damit HMR über HTTPS läuft)
     hmr: {
       host: '5173-ammarkaraja-former-08kqzylvsw0.ws-eu121.gitpod.io',
       protocol: 'wss',
